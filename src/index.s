dde9b3d86b281968403cddacdd17575639da4091
Champii <contact@champii.io>
2017-04-09 23:56:50 +0200

    Last fixup

--- src/index.s
+++ src/index.s
@@ -27,7 +27,6 @@ exports.transpile = (input, file) ->
   ast            = generateAst file, preprocessed
   transformedAst = transformAst ast
   transpiled     = transpile transformedAst
-  console.log transpiled
   compileFunc    = compile file
   compiled       = compileFunc transpiled
 
