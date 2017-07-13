467dedb2d3e287f90902478e0f363d384099772b
Champii <contact@champii.io>
2017-07-13 05:31:05 +0200

    add main test

---
+++ test/#main.js
@@ -0,0 +1,83 @@
+const chai           = require('chai');
+const gulp           = require('gulp');
+const Vinyl          = require('vinyl');
+const stream         = require('stream');
+const chaiAsPromised = require('chai-as-promised');
+
+const steel          = require('../');
+
+const expect         = chai.expect;
+
+chai.use(chaiAsPromised);
+
+describe('Main process', () => {
+
+  describe('Full compile', () => {
+
+    it('should recompile the whole project without errors', function (done) {
+      this.timeout(10000);
+      const outStream = steel.transpileStream(gulp.src('./src/*'));
+
+      outStream.on('data', (file) => {
+      });
+
+      outStream.on('error', (err) => {
+        done(new Error('Error'));
+      });
+
+      outStream.on('end', (err) => {
+        done();
+      });
+
+    });
+
+    it('should process file with no error', function (done) {
+      this.timeout(10000);
+      const pair = new Vinyl({ path: '/tmp/test.s', contents: new Buffer(`a = []`)});
+      const passStream = new stream.PassThrough({ objectMode: true });
+
+      const outStream = steel.transpileStream(passStream);
+
+      outStream.on('data', (file) => {
+        expect(file.path).to.eq('/tmp/test.js');
+        expect(file.contents.toString()).to.eq(`(function () {
+    let a = [];
+})();
+`);
+      });
+
+      outStream.on('error', (err) => {
+        done(new Error('Error'));
+      });
+
+      outStream.on('end', (err) => {
+        done();
+      });
+
+      passStream.push(pair);
+      passStream.push(null);
+    });
+
+    it('should process file with error', function (done) {
+      this.timeout(10000);
+      const pair = new Vinyl({ path: '/tmp/test.s', contents: new Buffer(`a: number = 'a'`)});
+      const passStream = new stream.PassThrough({ objectMode: true });
+
+      const outStream = steel.transpileStream(passStream);
+
+      outStream.on('data', (file) => {
+      });
+
+      outStream.on('error', (err) => {
+        done();
+        done = () => {};
+      });
+
+      passStream.push(pair);
+      passStream.push(null);
+
+    });
+
+  });
+
+});
